c2fe9a2d2f23078e26e8a6f938fc7aeac5cc969b
Prajwal Ghadge <prajwalghadge2005@gmail.com>
2025-12-10 05:58:32 +0530

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link rel="icon" href="/icon.png" />
       </head>
